e7b06bafacd9bafaa54394e217c1159d7de4f9c6
Mohit-chaudhary584 <mohit7838503206@gmail.com>
2025-06-11 11:41:26 +0530

    day 1

---
+++ level-1/KilometerToMiles.java
@@ -0,0 +1,8 @@
+class KilometerToMiles{
+	public static void main(String[] args){
+		float distanceInKilometer = 10.8f;
+		float kilometerToMiles = 1.6f;
+		float distanceInMiles = distanceInKilometer * kilometerToMiles;
+		System.out.println("The distance " + distanceInKilometer + " km in miles is " + distanceInMiles);
+	}
+}
