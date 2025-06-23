4c807e8361afc8a5dd4dc37fa6eac3395e949e72
Mohit-chaudhary584 <mohit7838503206@gmail.com>
2025-06-23 12:24:49 +0530

    [Mohit]add . added Circle.java

--- Java Programming Elements/CelsiusToFarenheit.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class CelsiusToFarenheit{
-	public static void main(String args[]){
-		Scanner sc = new Scanner(System.in);
-		float celsius = sc.nextFloat();
-		float farenheit = (celsius * 9 / 5) + 32;
-		System.out.print("The " + celsius + " celsius is " + farenheit + " fahrenheit");
-	}
-}
