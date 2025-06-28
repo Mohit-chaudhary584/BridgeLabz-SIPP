89daac9f9d2dc688571d801449f22eeea694616e
Mohit-chaudhary584 <mohit7838503206@gmail.com>
2025-06-28 09:53:33 +0530

    changed default branch

--- Java Programming Elements/FahrenheitToCelsius.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class FahrenheitToCelsius{
-	public static void main(String args[]){
-		Scanner sc = new Scanner(System.in);
-		float farenheit = sc.nextFloat();
-		float celsius = (farenheit - 32) * (5 / 9);
-		System.out.print("The " + farenheit + " fahrenheit is " + celsius + " celsius");
-	}
-}
--- level-1/HarryAge.java
+++
@@ -1,9 +0,0 @@
-class HarryAge{
-	public static void main(String[] args){
-		String name = "Harry";
-		int birthYear = 2000;
-		int currentYear = 2024;
-		int currentAge = birthYear - currentYear;
-		System.out.println(name + "'s age in " + currentYear + " is " + currentAge); 
-	}
-}
