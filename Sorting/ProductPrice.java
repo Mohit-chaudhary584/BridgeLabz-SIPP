cc38229f14ad2a5e03fc52b5eb98f2df74036188
Mohit-chaudhary584 <mohit7838503206@gmail.com>
2025-07-09 12:20:00 +0530

    [Mohit]add . added ProductPrice.java

---
+++ Sorting/ProductPrice.java
@@ -0,0 +1,36 @@
+package sorting;
+
+public class ProductPrice {
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+    	int[] price = {249, 199, 99, 149, 499, 49};
+        quickSort(price, 0, price.length - 1);
+        for(int x : price) {
+        	System.out.print(x+" ");
+        }
+    }
+}
