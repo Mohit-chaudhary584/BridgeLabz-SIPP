93712882f6a092bdb1a015882c10e76dc42f8d0c
Mohit-chaudhary584 <mohit7838503206@gmail.com>
2025-07-22 09:52:18 +0530

    [Mohit]add . added BookBuddy.java

--- Java Programming Elements/HandShake.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class HandShake{
-	public static void main(String args[]){
-		Scanner sc = new Scanner(System.in);
-		int numberOfStudents = sc.nextInt();
-		int combination = (numberOfStudents * (numberOfStudents - 1)) / 2;
-		System.out.print("Number of handshakes " + combination);
-	}
-}
--- level-1/KilometerToMiles.java
+++
@@ -1,8 +0,0 @@
-class KilometerToMiles{
-	public static void main(String[] args){
-		float distanceInKilometer = 10.8f;
-		float kilometerToMiles = 1.6f;
-		float distanceInMiles = distanceInKilometer * kilometerToMiles;
-		System.out.println("The distance " + distanceInKilometer + " km in miles is " + distanceInMiles);
-	}
-}
